5281bdd5c6d48d3c0b600c7adc188febc7064159
juliolopez254 <1133276@iheartmedia.com>
2022-07-21 15:41:13 -0500

    react-app-setup

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (
+        <>
+        <div>
+            <p>
+                React app.
+            </p>
+        </div>
+        </>
+    )
+}
+
+export default Home
