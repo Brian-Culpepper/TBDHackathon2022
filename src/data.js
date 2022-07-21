aad460ba06eb92f76aca3d6eff493f8bf8635d3c
katrinaBaxter <106756768+katrinaBaxter@users.noreply.github.com>
2022-07-21 17:20:14 -0400

    created src/data.js and added abba lyrics

---
+++ src/data.js
@@ -0,0 +1,158 @@
+export const abbaLyrics = [
+    {
+      "seconds": 2,
+      "lyrics": "(upbeat disco music)"
+    },
+    {
+      "seconds": 19,
+      "lyrics": "Ooh, ooh"
+    },
+    {
+      "seconds": 22,
+      "lyrics": "You can dance, you can jive "
+    },
+    {
+      "seconds": 27,
+      "lyrics": "Having the time of your life "
+    },
+    {
+      "seconds": 30,
+      "lyrics": "Ooh, see that girl, watch that scene "
+    },
+    {
+      "seconds": 35,
+      "lyrics": "Diggin' the dancing queen "
+    },
+    {
+      "seconds": 46,
+      "lyrics": "Friday night and the lights are low "
+    },
+    {
+      "seconds": 51,
+      "lyrics": "Looking out for a place to go "
+    },
+    {
+      "seconds": 55,
+      "lyrics": "Where they play the right music "
+    },
+    {
+      "seconds": null,
+      "lyrics": "Getting in the swing "
+    },
+    {
+      "seconds": 59,
+      "lyrics": "You come to look for a king "
+    },
+    {
+      "seconds": 65,
+      "lyrics": "Anybody could be that guy "
+    },
+    {
+      "seconds": 69,
+      "lyrics": "Night is young and the music's high "
+    },
+    {
+      "seconds": 74,
+      "lyrics": "With a bit of rock music "
+    },
+    {
+      "seconds": 77,
+      "lyrics": "Everything is fine "
+    },
+    {
+      "seconds": 79,
+      "lyrics": "You're in the mood for a dance "
+    },
+    {
+      "seconds": 83,
+      "lyrics": "And when you get the chance "
+    },
+    {
+      "seconds": 87,
+      "lyrics": "You are the dancing queen "
+    },
+    {
+      "seconds": 91,
+      "lyrics": "Young and sweet, only 17 "
+    },
+    {
+      "seconds": 98,
+      "lyrics": "Dancing queen "
+    },
+    {
+      "seconds": 101,
+      "lyrics": "Feel the beat from the tambourine, oh yeah "
+    },
+    {
+      "seconds": 108,
+      "lyrics": "You can dance, you can jive "
+    },
+    {
+      "seconds": 113,
+      "lyrics": "Having the time of your life "
+    },
+    {
+      "seconds": 116,
+      "lyrics": "Ooh, see that girl, watch that scene "
+    },
+    {
+      "seconds": 121,
+      "lyrics": "Diggin' the dancing queen "
+    },
+    {
+      "seconds": 136,
+      "lyrics": "You're a teaser, you turn 'em on "
+    },
+    {
+      "seconds": 141,
+      "lyrics": "Leave 'em burning and then you're gone "
+    },
+    {
+      "seconds": 146,
+      "lyrics": "Looking out for another, anyone will do "
+    },
+    {
+      "seconds": 150,
+      "lyrics": "You're in the mood for a dance "
+    },
+    {
+      "seconds": 154,
+      "lyrics": "And when you get the chance "
+    },
+    {
+      "seconds": 159,
+      "lyrics": "You are the dancing queen "
+    },
+    {
+      "seconds": 163,
+      "lyrics": "Young and sweet, only 17 "
+    },
+    {
+      "seconds": 170,
+      "lyrics": "Dancing queen "
+    },
+    {
+      "seconds": 172,
+      "lyrics": "Feel the beat from the tambourine, oh yeah "
+    },
+    {
+      "seconds": 179,
+      "lyrics": "You can dance, you can jive "
+    },
+    {
+      "seconds": 184,
+      "lyrics": "Having the time of your life "
+    },
+    {
+      "seconds": 188,
+      "lyrics": "Oh, see that girl, watch that scene "
+    },
+    {
+      "seconds": 193,
+      "lyrics": "Diggin' the dancing queen "
+    },
+    {
+      "seconds": 203,
+      "lyrics": "Diggin' the dancing queen "
+    }
+  ]
