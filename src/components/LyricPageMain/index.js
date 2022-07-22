29637a0d712825959279fb952fccafda4bb26045
juliolopez254 <1133276@iheartmedia.com>
2022-07-21 22:14:38 -0500

    fix

--- src/components/LyricPageMain/index.js
+++ src/components/LyricPageMain/index.js
@@ -1,13 +1,25 @@
 import './index.css'
+import {abbaLyrics, LOAPV1, LOAPV2} from "../../data.js"
+
+const songs = [abbaLyrics, LOAPV1, LOAPV2];
 
 const LyricPageMain = () => {
+    
     return (
         <>
-        <div>
-            <p>BRENT REEVES.</p>
+        <div className="lyric-container">
+            {songs[0].map((data, key) => {
+            return (
+                <div key={key}>
+                {data.lyrics}
+                </div>
+            );
+            })}
         </div>
         </>
-    )
+    );
+
+    
 }
 
 export default LyricPageMain
--- src/components/MainPageCenter/index.js
+++ src/components/MainPageCenter/index.js
@@ -1,16 +1,54 @@
-import './index.css'
-import {Link} from '../../../node_modules/react-router-dom/index'
+import "./index.css"
+import like from "../MainPageCenter/Like.png"
+import dislike from "../MainPageCenter/Dislike.png"
+import React, { useState, useEffect } from "react";
+import url from "../MainPageCenter/Abba_-_Dancing_Queen_Official_Musi_(getmp3.pro).mp3"
+import { Link } from "react-router-dom";
+
+const useAudio = url => {
+    const [audio] = useState(new Audio(url));
+    const [playing, setPlaying] = useState(false);
+  
+    const toggle = () => setPlaying(!playing);
+  
+    useEffect(() => {
+        playing ? audio.play() : audio.pause();
+      },
+      [playing]
+    );
+    useEffect(() => {
+        audio.addEventListener('ended', () => setPlaying(false));
+        return () => {
+          audio.removeEventListener('ended', () => setPlaying(false));
+        };
+      }, []);
+      useEffect(() => {
+        audio.addEventListener('ended', () => setPlaying(false));
+        return () => {
+          audio.removeEventListener('ended', () => setPlaying(false));
+        };
+      }, []);
+    
+      return [playing, toggle];
+    };
+    
 
 const MainPageCenter = () => {
+    const [playing, toggle] = useAudio(url);
     return (
         <>
         <div>
             {/* <button type="button" className="container lyric-btn">Lyrics</button> */}
-            <Link to='/LyricPageMain' className='LyricButton'>Lyrics</Link>
-        </div>
-        <div>
-            <p className="container SongName">Mean Mr. Mustard</p>
-            <p className="container Artist">The Beatles</p>
+            <Link to="/LyricPageMain">Lyrics</Link>
+            <div className="container like-dislike">
+                <input type="image" src={dislike} alt="dislike button" className="container dislike"/>
+                <input type="image" src={like} alt="like button" className="container like"/>
+            </div>
+            <div className="container pause-play">
+                <button type="button" onClick={toggle}>{playing ? "Pause" : "Play"}</button>
+            </div>
+            <p className="container SongName">Dancing Queen</p>
+            <p className="container Artist">ABBA</p>
         </div>
         </>
     )
