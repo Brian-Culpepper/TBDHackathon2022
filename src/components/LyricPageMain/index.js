b5f72304a30e8e0436be188f8a92cd60422e9214
Brian-Culpepper <1133288@iHeartMedia.com>
2022-07-22 07:33:38 -0500

    wip

--- src/components/LyricPageMain/index.js
+++ src/components/LyricPageMain/index.js
@@ -1,6 +1,8 @@
 import './index.css'
 import {abbaLyrics, LOAPV1, LOAPV2} from "../../data.js"
 import Footer from '../../components/Footer'
+import home from '../../components/AlbumCover/ABBA.png'
+import { Link } from "react-router-dom";
 
 const songs = [abbaLyrics, LOAPV1, LOAPV2];
 
@@ -8,6 +10,13 @@ const LyricPageMain = () => {
     
     return (
         <>
+        <div className='lyricHeader'>
+            <Link className='home' to="/" ><input type="image" name="skip" src={home} width='75' height='75' alt='skip'/></Link>
+            <div className='SongInfo'>
+                <p className="container SongName">Dancing Queen</p>
+                <p className="container Artist">ABBA</p>
+            </div>
+        </div>
         <div className="lyric-container">
             {songs[0].map((data, key) => {
             return (
