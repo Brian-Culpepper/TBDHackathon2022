143b4692142ce38d1e4538bf568c653b5f881ad6
Brian-Culpepper <1133288@iHeartMedia.com>
2022-07-22 03:38:12 -0500

    wip

--- src/components/LyricPageMain/index.js
+++ src/components/LyricPageMain/index.js
@@ -1,5 +1,8 @@
 import './index.css'
-import {abbaLyrics, LOAPV1, LOAPV2} from "../../data.js"
+import {abbaLyrics, LOAPV1, LOAPV2} from "../../data.js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid} from '@fortawesome/fontawesome-svg-core/import.macro'
+
 
 const songs = [abbaLyrics, LOAPV1, LOAPV2];
 
@@ -7,6 +10,7 @@ const LyricPageMain = () => {
     
     return (
         <>
+        <button type='button'>Return to Song</button>
         <div className="lyric-container">
             {songs[0].map((data, key) => {
             return (
