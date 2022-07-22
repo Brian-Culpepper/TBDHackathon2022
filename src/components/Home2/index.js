d82a6009948b33d34cd2461b5af4f3302ae45f0a
Brian-Culpepper <1133288@iHeartMedia.com>
2022-07-22 02:39:46 -0500

    Adds BonJovi pages

--- src/components/Home2/index.js
+++ src/components/Home2/index.js
@@ -1,8 +1,20 @@
 import './index.css'
+import SongInfo from '../SongInfo'
+import MainPageCenter2 from '../MainPageCenter2'
+import AlbumCover2 from '../AlbumCover2'
+import Footer from '../Footer'
 
 const Home2 = () => {
     return (
         <>
+        <html className='main-page'>
+            <div>
+                <SongInfo/>
+                <AlbumCover2/> 
+                <MainPageCenter2/>
+                <Footer/>
+            </div>
+        </html>
         </>
     )
 }
