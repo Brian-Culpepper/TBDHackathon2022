cac7ce6ffed97a90b126cd75dd3b762a33a983ac
juliolopez254 <1133276@iheartmedia.com>
2022-07-22 03:38:45 -0500

    update

--- src/components/MainPageCenter/index.js
+++ src/components/MainPageCenter/index.js
@@ -1,40 +1,9 @@
 import "./index.css"
 import like from "../MainPageCenter/Like.png"
 import dislike from "../MainPageCenter/Dislike.png"
-import React, { useState, useEffect } from "react";
-import url from "../MainPageCenter/Abba_-_Dancing_Queen_Official_Musi_(getmp3.pro).mp3"
-import { Link } from "react-router-dom";
-
-const useAudio = url => {
-    const [audio] = useState(new Audio(url));
-    const [playing, setPlaying] = useState(false);
-  
-    const toggle = () => setPlaying(!playing);
-  
-    useEffect(() => {
-        playing ? audio.play() : audio.pause();
-      },
-      [playing]
-    );
-    useEffect(() => {
-        audio.addEventListener('ended', () => setPlaying(false));
-        return () => {
-          audio.removeEventListener('ended', () => setPlaying(false));
-        };
-      }, []);
-      useEffect(() => {
-        audio.addEventListener('ended', () => setPlaying(false));
-        return () => {
-          audio.removeEventListener('ended', () => setPlaying(false));
-        };
-      }, []);
-    
-      return [playing, toggle];
-    };
-    
+import { Link } from "react-router-dom";    
 
 const MainPageCenter = () => {
-    const [playing, toggle] = useAudio(url);
     return (
         <>
         <div>
@@ -44,9 +13,7 @@ const MainPageCenter = () => {
                 <input type="image" src={dislike} alt="dislike button" className="container dislike"/>
                 <input type="image" src={like} alt="like button" className="container like"/>
             </div>
-            <div className="container pause-play">
-                <button type="button" onClick={toggle}>{playing ? "Pause" : "Play"}</button>
-            </div>
+            <br></br>
             <p className="container SongName">Dancing Queen</p>
             <p className="container Artist">ABBA</p>
         </div>
