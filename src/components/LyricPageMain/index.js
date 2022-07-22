701a831d814b875fbab185c8ba4a4ed36de9871a
katrinaBaxter <106756768+katrinaBaxter@users.noreply.github.com>
2022-07-22 08:28:09 -0400

    Revert "added scroll functionality to lyrics"
    This reverts commit 9f82baa93dfc0be58066dc2b2ea6a5797ac2760f.

--- src/components/LyricPageMain/index.js
+++ src/components/LyricPageMain/index.js
@@ -1,33 +1,30 @@
 import './index.css'
 import {abbaLyrics, LOAPV1, LOAPV2} from "../../data.js"
-import $ from 'jquery'
+import Footer from '../../components/Footer'
 
 const songs = [abbaLyrics, LOAPV1, LOAPV2];
 
 const LyricPageMain = () => {
-    let totalTime = 0;
+    
     return (
         <>
-         <marquee direction='up' behavior="scroll" scrolldelay="500" height="100px">
+        <div className="lyric-container">
             {songs[0].map((data, key) => {
-                let delay = data.seconds - totalTime;
-                totalTime += data.seconds;
-                return (
-                   
-                    <div id="div"> 
-                        {data.lyrics}
-                    </div> 
-                    
-                        
-                );
+            return (
+                <div className='lyrics' key={key}>
+                {data.lyrics}
+                </div>
+            );
             })}
-            </marquee>
+        </div>
+        <br></br>
+        <div className='footer-div'>
+            <Footer/>
+        </div>
         </>
     );
 
     
 }
-
-
 
 export default LyricPageMain
